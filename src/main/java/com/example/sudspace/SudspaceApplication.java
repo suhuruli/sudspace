4733e62ff9c58dc04243598115587d68ca89674c
Sudhanva Huruli <sudhanvahuruli@gmail.com>
2018-05-26 21:01:09 -0700

    initial commit

---
+++ src/main/java/com/example/sudspace/SudspaceApplication.java
@@ -0,0 +1,12 @@
+package com.example.sudspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudspaceApplication.class, args);
+	}
+}
